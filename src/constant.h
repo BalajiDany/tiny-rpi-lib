dcb6bac29de74cd6749360e215fd702ec6774ce2
Balaji D <d.balaji.mc@gmail.com>
2018-02-10 16:40:26 +0530

    'tiny-rpi-lib'

---
+++ src/constant.h
@@ -0,0 +1,13 @@
+/*
+ *	constant.h
+ *	Hold all the necessary constant for libraries in current directory.
+ *	Additionally it provides pin mapping for each library.
+ *
+ *	Created on	: Feb 10, 2018
+ *	Author		: Balaji
+ *	Email 		: d.balaji.mc@gmail.com
+ */
+#ifndef CONSTANT_H_
+#define CONSTANT_H_
+
+#endif /* CONSTANT_H_ */
---
+++ src/engine.h
@@ -0,0 +1,13 @@
+/*
+ * 	engine.h
+ * 	Includes all the library in present directory
+ *
+ *	Created on	: Feb 10, 2018
+ *	Author		: Balaji
+ *	Email		: d.balaji.mc@gmail.com
+ */
+
+#ifndef ENGINE_H_
+#define ENGINE_H_
+
+#endif /* ENGINE_H_ */
